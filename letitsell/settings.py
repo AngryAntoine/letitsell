a376faaf2710dea4c600efa911682b649047da22
Anton Gramenko <tony1986@yandex.ru>
2016-05-24 13:08:20 +0300

    Update settings.py

--- letitsell/settings.py
+++ letitsell/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e8v7^@j-mok44_tyus4xju@qpn)to816!^(16@&!d7#^4c^f5!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
